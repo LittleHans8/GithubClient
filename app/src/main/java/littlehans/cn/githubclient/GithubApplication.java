b003356ecac61e2a25efe22279973d23bea1b663
谭瀚涛 <littlehans8@gmail.com>
2016-09-27 19:21:37 +0800

    Initial commit

---
+++ app/src/main/java/littlehans/cn/githubclient/GithubApplication.java
@@ -0,0 +1,11 @@
+package littlehans.cn.githubclient;
+
+import android.app.Application;
+
+/**
+ * Created by LittleHans on 2016/9/27.
+ */
+
+public class GithubApplication extends Application {
+
+}
---
+++ app/src/main/java/littlehans/cn/githubclient/ui/search/SearchActivity.java
@@ -0,0 +1,8 @@
+package littlehans.cn.githubclient.ui.search;
+
+/**
+ * Created by LittleHans on 2016/9/27.
+ */
+
+public class SearchActivity {
+}
