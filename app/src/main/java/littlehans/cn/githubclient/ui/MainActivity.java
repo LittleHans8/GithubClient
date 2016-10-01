e2ea8f66c6c5fe195b1111d4cb0cf0270e64a49b
谭瀚涛 <littlehans8@gmail.com>
2016-10-01 22:40:49 +0800

    update resources

--- app/src/main/java/littlehans/cn/githubclient/ui/MainActivity.java
+++ app/src/main/java/littlehans/cn/githubclient/ui/MainActivity.java
@@ -18,7 +18,8 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends BaseActivity
+    implements NavigationView.OnNavigationItemSelectedListener {
 
   private static final String TAG = "MainActivity";
   @BindView(R.id.toolbar) Toolbar mToolbar;
@@ -31,22 +32,24 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
     setSupportActionBar(mToolbar);
 
     ActionBarDrawerToggle toggle =
-        new ActionBarDrawerToggle(this, mDrawer, mToolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        new ActionBarDrawerToggle(this, mDrawer, mToolbar, R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close);
     mDrawer.setDrawerListener(toggle);
     toggle.syncState();
 
     mNavigationView.setNavigationItemSelectedListener(this);
-    final Call<Search> repos = GithubService.createSearchService().repositories("bootsrap",null,null);
+    final Call<Search> repos =
+        GithubService.createSearchService().repositories("bootsrap", null, null);
     repos.enqueue(new Callback<Search>() {
       @Override public void onResponse(Call<Search> call, Response<Search> response) {
-        for(Search.Items items : response.body().items) {
+        for (Search.Items items : response.body().items) {
           Log.v(TAG, "onResponse: " + items.toString());
         }
         Log.v(TAG, "onResponse: " + response.message());
       }
 
       @Override public void onFailure(Call<Search> call, Throwable t) {
-        for(StackTraceElement errorMessage: t.getStackTrace()) {
+        for (StackTraceElement errorMessage : t.getStackTrace()) {
           Log.v(TAG, "onFailure: " + errorMessage.toString());
         }
 
@@ -65,7 +68,7 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
 
   @Override public boolean onCreateOptionsMenu(Menu menu) {
     // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.main, menu);
+    getMenuInflater().inflate(R.menu.search, menu);
     return true;
   }
 
@@ -80,7 +83,8 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
     return super.onOptionsItemSelected(item);
   }
 
-  @SuppressWarnings("StatementWithEmptyBody") @Override public boolean onNavigationItemSelected(MenuItem item) {
+  @SuppressWarnings("StatementWithEmptyBody") @Override
+  public boolean onNavigationItemSelected(MenuItem item) {
     // Handle navigation view item clicks here.
     int id = item.getItemId();
 
