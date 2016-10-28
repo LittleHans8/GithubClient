6871b80c5d1dbabd7574ada66d8716a35360757d
谭瀚涛 <littlehans8@gmail.com>
2016-10-28 20:03:07 +0800

    add comment service

--- app/src/main/java/littlehans/cn/githubclient/api/service/IssuesService.java
+++ app/src/main/java/littlehans/cn/githubclient/api/service/IssuesService.java
@@ -2,6 +2,7 @@ package littlehans.cn.githubclient.api.service;
 
 import android.support.annotation.Nullable;
 import java.util.List;
+import littlehans.cn.githubclient.model.entity.Comment;
 import littlehans.cn.githubclient.model.entity.Issue;
 import retrofit2.Call;
 import retrofit2.http.GET;
@@ -10,6 +11,10 @@ import retrofit2.http.Query;
 
 /**
  * Created by LittleHans on 2016/10/26.
+ */
+
+/**
+ * https://developer.github.com/v3/issues/
  */
 
 public interface IssuesService {
@@ -25,4 +30,11 @@ public interface IssuesService {
   @GET("/repos/{owner}/{repo}/issues") Call<List<Issue>> getIssuesForReposList(
       @Path("owner") String owner, @Path("repo") String repo,
       @Nullable @Query("state") String state, @Query("page") int page);
+
+  /**
+   * GET /repos/:owner/:repo/issues/:number/comments
+   */
+  @GET("/repos/{owner}/{repo}/issues/{number}/comments") Call<List<Comment>> getComments(
+      @Path("owner") String owner, @Path("repo") String repo, @Path("number") String number,
+      @Query("page") int page);
 }
